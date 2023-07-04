c7e8e71104342bd19f218b66f09cbe7127838885
yurigarrido <y.garrido.yuri@gmail.com>
2023-07-03 21:09:29 -0300

    chore: add eslint config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,28 @@
-import { Head, Html, Main, NextScript } from "next/document";
-import {getCssText} from '@ignite-ui/react'
+import { Head, Html, Main, NextScript } from 'next/document'
+import { getCssText } from '@ignite-ui/react'
 export default function Document() {
   return (
     <Html>
       <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
 
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
-          <style id='stitches' dangerouslySetInnerHTML={{__html: getCssText()}}></style>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        ></style>
       </Head>
       <body>
-        <Main/>
-        <NextScript/>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   )
