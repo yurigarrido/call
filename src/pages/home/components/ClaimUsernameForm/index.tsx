c41032889109f6eca12226f71fa39a16b1e4816f
yurigarrido <y.garrido.yuri@gmail.com>
2023-07-04 21:24:21 -0300

    feat: add react hook form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,10 +1,30 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { Form } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleCalimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="md" prefix="call.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleCalimUsername)}>
+      <TextInput
+        size="md"
+        prefix="call.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar usuário
         <ArrowRight />
