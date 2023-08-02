446f6cd8d0e9eb9f4d3703cbc14c8a03abd67efe
Yuri Garrid <y.garrido.yuri@gmail.com>
2023-08-02 19:35:00 -0300

    fix: block past horus

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -69,10 +69,21 @@ export default async function handler(
     },
   })
 
+  // const availableTimes = possibleTimes.filter((time) => {
+  //   const isTimeBlocked = !blockedTimes.some(
+  //     (blockedTime) => blockedTime.date.getHours() === time,
+  //   )
+  //   const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date())
+
+  //   return false
+  // })
+
   const availableTimes = possibleTimes.filter((time) => {
-    return !blockedTimes.some(
+    const isTimeBlocked = blockedTimes.some(
       (blockedTime) => blockedTime.date.getHours() === time,
     )
+    const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date())
+    return !isTimeBlocked && !isTimeInPast
   })
 
   return res.json({ possibleTimes, availableTimes })
